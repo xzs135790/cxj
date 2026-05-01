e21e13b91c71f35fad970e16296ce2a13b32bfd7
xzs135790 <3639676338@qq.com>
2026-05-01 11:10:45 +0800

    Delete js directory

--- js/tan.js
+++
@@ -1,6 +0,0 @@
-const tan = document.getElementById("tan")
-const ann = document.getElementById("ann");
-tan.style.display = "block";
-ann.addEventListener('click', function () {
-    tan.style.display = "none";
-})
